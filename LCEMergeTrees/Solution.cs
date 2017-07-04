8b3d5c2929e19e3dab3bda72bb115cdd2440d23b
aviverit <andrew.viverito@gmail.com>
2017-07-04 16:35:19 -0500

    v.0.1.1 "untested"

--- LCEMergeTrees/Solution.cs
+++ LCEMergeTrees/Solution.cs
@@ -11,12 +11,25 @@ namespace LCEMergeTrees
         public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
         {
             int rootVal;
-            if (t1.val != null && t2.val != null) {
+            if (t1.val != null && t2.val != null)
+            {
                 rootVal = t1.val + t2.val;
 
             }
+            else if (t1.val != null)
+            {
+                rootVal = t1.val;
+            }
+            else
+            {
+                rootVal = t2.val;
+            }
             var tempResult = new TreeNode(rootVal);
-            while (t1.left != null) { }
+            if (t1.left.val != null || t2.left.val != null) {
+                MergeTrees(t1.left, t2.left);
+            } else if (t1.right.val != null || t2.right.val != null) {
+                MergeTrees(t1.right, t2.right);
+            }
             return tempResult;
         }
     }
